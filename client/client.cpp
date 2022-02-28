8f1e9658fddc0fbc94e163beed22034d572fce20
Martin Lefevre <lefevre.martin@outlook.com>
2022-02-28 11:35:44 +0100

    low battery save

--- client/client.cpp
+++ client/client.cpp
@@ -13,7 +13,6 @@
 #include <poll.h>
 
 #define PORT 4269
-#define sockaddr_in struct sockaddr_in
 #define SA struct sockaddr
 
 using namespace std;
@@ -33,43 +32,21 @@ int main()
 {
 	int sockfd = socket(AF_INET, SOCK_STREAM , 0);
 	if (sockfd < 0) {
-		cout << "Couldn't open socket" << endl;
+		cerr << "Couldn't open socket" << endl;
 		return SOCKET_ERROR;
 	}
-	cout << "Successfully created socket" << endl;
-
+	cerr << "Successfully created socket" << endl;
 	sockaddr_in server_addr;
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family		= AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server_addr.sin_port		= htons(PORT);
-
-
-	// if (fcntl(sockfd, F_SETFL, O_NONBLOCK) < 0) {
-	// 	cout << "Couldn't set O_NONBLOCK" << endl;
-	// 	close(sockfd);
-	// }
-	// cout << "O_NONBLOCK set" << endl;
-
-	#define pollfd struct pollfd
-	// pollfd server_fd;
-	// server_fd.fd = sockfd;
-	// server_fd.events = POLLOUT; 
-	// int rval = -1;
-	// cout << "coucou\n";
-	// 	while (rval <= 0) {
-	// 		rval = poll(&server_fd, 1, 1);
-	// 	}
-	// cout << "rval " << rval << endl;
-
-
 	if (connect(sockfd, (SA*)&server_addr, sizeof(server_addr)) < 0) {
-		cout << "Coudln't connect" << endl;
-		cout << "errno = " << errno << endl;
+		cerr << "Coudln't connect" << endl;
+		cerr << "errno = " << errno << endl;
 		return CONNECT_ERROR;
 	}
-	cout << "Successfully connected" << endl;
-
+	cerr << "Successfully connected" << endl;
 	chat(sockfd);
 	close(sockfd);
 	return 0;
